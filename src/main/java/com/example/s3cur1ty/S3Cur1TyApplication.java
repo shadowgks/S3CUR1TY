5e11521f08c6197a3c3d4ba967b528624cf5a69c
shadowgks <saad.moumou.coder@gmail.com>
2024-01-30 12:07:27 +0100

    prepare environment

---
+++ src/main/java/com/example/s3cur1ty/S3Cur1TyApplication.java
@@ -0,0 +1,13 @@
+package com.example.s3cur1ty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3Cur1TyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S3Cur1TyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/s3cur1ty/S3Cur1TyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s3cur1ty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3Cur1TyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
